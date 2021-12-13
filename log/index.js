55e1fe2b323d56dcffd9855f48dff733b4062fef
liyang <yang.li@xyzrobotics.ai>
2021-12-13 10:27:47 +0800

    init

---
+++ log/index.js
@@ -0,0 +1,15 @@
+const { FitAddon } = require('xterm-addon-fit')
+const {ipcRenderer} = require('electron')
+
+var term = new Terminal();
+const fitAddon = new FitAddon();
+term.loadAddon(fitAddon);
+term.open(document.getElementById('terminal'));
+fitAddon.fit();
+ipcRenderer.on('send-backend-log', (e, arg) => {
+    term.writeln(arg);
+})
+
+window.addEventListener('resize', function() {
+    fitAddon.fit();
+})
---
+++ log/test.py
@@ -0,0 +1,6 @@
+import time
+import sys
+
+for i in range(5):
+    print('hi')
+    time.sleep(1)
